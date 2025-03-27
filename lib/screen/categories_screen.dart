cc358e1be3f7e731615cbca31deef35fc5ad3397
Bonifacio Conde <b.conde@foodics.com>
2025-03-27 16:15:53 +0800

    Add dummy data and display grid view

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -7,7 +7,24 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Categories")),
-      body: Text("data"),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          childAspectRatio: 1.5,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text(
+            textAlign: TextAlign.center,
+            "1",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text("2", style: TextStyle(color: Colors.white)),
+          Text("3", style: TextStyle(color: Colors.white)),
+          Text("4", style: TextStyle(color: Colors.white)),
+        ],
+      ),
     );
   }
 }
